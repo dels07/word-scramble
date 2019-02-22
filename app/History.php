59d9e0cc26428506cb29ed139e7edc0820850811
deli.oona <deli.oona@gmail.com>
2019-02-22 13:59:54 +0700

    Add fillable and relation belongsTo to user and word

--- app/History.php
+++ app/History.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class History extends Model
 {
-    //
+    protected $fillable = ['user_id', 'word_id', 'point'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
+    public function word()
+    {
+        return $this->belongsTo('App\Word', 'word_id');
+    }
 }
