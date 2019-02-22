83695e57ab5623599b6df8fe2ee294eff91bf42d
deli.oona <deli.oona@gmail.com>
2019-02-22 13:57:57 +0700

    Add scope admin/user, add history relation

--- app/User.php
+++ app/User.php
@@ -10,30 +10,27 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $fillable = ['name', 'email', 'password', 'is_admin', 'point', 'words'];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token'];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'is_admin'          => 'boolean'
     ];
+
+    public function scopeisAdmin($query)
+    {
+        return $query->where('is_admin', 1);
+    }
+
+    public function scopeisUser($query)
+    {
+        return $query->where('is_admin', 0);
+    }
+
+    public function histories()
+    {
+        return $this->hasMany('App\History', 'user_id', 'id');
+    }
 }
