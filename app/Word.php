f41da3818c34c20ae2917a1b4ffcc303b8d75409
deli.oona <deli.oona@gmail.com>
2019-02-22 13:59:08 +0700

    Add fillable and relation belongsTo to category

--- app/Word.php
+++ app/Word.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Word extends Model
 {
-    //
+    protected $fillable = ['word', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo('App\Category', 'category_id');
+    }
+
+    public function getWordAttribute($value)
+    {
+        return str_shuffle($value);
+    }
 }
