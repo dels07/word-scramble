69bb3fb4079bbe685eef9529c19df3f7d1e5a351
deli.oona <deli.oona@gmail.com>
2019-02-22 13:58:25 +0700

    Add fillable and hasMany relation to word

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = ['name'];
+
+    public function words()
+    {
+        return $this->hasMany('App\Word', 'category_id', 'id');
+    }
 }
