4fc92968e1e22ebdc94369a21cffe7d8883e7501
deli.oona <deli.oona@gmail.com>
2019-02-26 15:52:24 +0700

    Redirect user to login if not admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,7 +7,14 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function index() {
+    public function index()
+    {
+        if (auth()->user()->is_admin == false) {
+            auth()->logout();
+
+            return redirect(route('auth.login'));
+        }
+
         $histories = History::orderByDesc('created_at')->paginate(25);
 
         return view('admin')->with('histories', $histories);
